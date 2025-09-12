9f26956bbf340036bd25e1649d5688e77a5665d3
rinshida <rinshidav0706@gmail.com>
2025-09-12 15:20:33 +0530

    new update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,8 +5,8 @@ const Profile = () => {
     <div className=' flex flex-col w-screen bg-blue-950'>
 
        <div className=' w-screen flex'>
-        <div><img className=' w-[150px] h-[150px] rounded-[50px]' src="" alt="" /></div>
-        <div>
+        <div><img className=' w-[150px] h-[150px] rounded-[50px] bg-gray-50' src="" alt="" /></div>
+        <div className=' flex gap-[200px] items-center'>
             <div className=' flex flex-col'>
                 <h1>posts</h1>
                 <h1>123</h1>
@@ -18,7 +18,14 @@ const Profile = () => {
            
         </div>
        </div>
-       <div className=' w-screen'></div>
+       <div className=' w-screen'>
+        {[1,2,3,4,5].map((ele,i)=>{
+            return( 
+            <div  className=' w-[250px] h-[330px] flex flex-col gap-2 overflow-hidden border rounded-xl border-white  bg-white p-5' key={i}>
+            <h1>post</h1>
+            </div>)
+})}
+       </div>
 
     </div>
   )

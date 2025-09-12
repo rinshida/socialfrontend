94f9bc286ba11945c45ce4744a91666fa8b545b4
rinshida <rinshidav0706@gmail.com>
2025-09-12 15:31:45 +0530

    new update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,10 +4,10 @@ const Profile = () => {
   return (
     <div className=' flex flex-col gap-[20px] w-screen bg-blue-950 p-[30px]'>
 
-       <div className=' w-screen flex'>
+       <div className=' w-screen flex gap-[20px]'>
         <div><img className=' w-[100px] h-[100px] rounded-[50px] bg-gray-50' src="" alt="" /></div>
-        <div className=' flex gap-[200px] items-center'>
-            <div className=' flex flex-col'>
+        <div className=' flex gap-[200px] items-center text-2xl text-white'>
+            <div className=' flex flex-col '>
                 <h1>posts</h1>
                 <h1>123</h1>
             </div>
@@ -15,10 +15,15 @@ const Profile = () => {
                 <h1>followers</h1>
                 <h1>123</h1>
             </div>
+            <div className=' flex flex-col'>
+                <h1>following</h1>
+                <h1>123</h1>
+            </div>
+
            
         </div>
        </div>
-       <div className=' grid lg:grid-cols-4 sm:grid-cols-1 gap-5'>
+       <div className=' grid lg:grid-cols-4 sm:grid-cols-1 gap-3 '>
         {[1,2,3,4,5].map((ele,i)=>{
             return( 
             <div  className=' w-[250px] h-[330px] flex flex-col gap-2 overflow-hidden border rounded-xl border-white  bg-white p-5' key={i}>

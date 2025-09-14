a9c173225be894ca7a645ed8edbde05c3a4a4da6
rinshida <rinshidav0706@gmail.com>
2025-09-14 20:50:17 +0530

    updated profile API

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -117,8 +117,8 @@ const Profile = () => {
             return( 
                 <div   className=' w-[250px] h-[330px] flex flex-col gap-2 overflow-hidden border rounded-xl border-white  bg-white p-5' key={j}>
                               <div className=' flex gap-2 items-center h-[20px] '>
-                                <img className=' w-[20px] h-[20px] rounded-[50px] bg-gray-100' src={post.userId.profile} alt="img" />
-                                <h1>{post.userId.name}</h1></div>
+                                {user && <img className=' w-[20px] h-[20px] rounded-[50px] bg-gray-100' src={user.profile} alt="img" />}
+                                {user && <h1>{user.name}</h1>}</div>
                               <div className=' flex flex-col justify-between '><div className='  w-[250px] '>
                                 <div className=' w-[200px] h-[200px] overflow-hidden'><div className=' w-[200px] h-[200px] duration-700 flex '
                                   style={{ transform: `translateX(-${(carosal[j] || 0) * 100}%)` }}>
